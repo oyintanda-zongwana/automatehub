cb69e119cb4d7ccb48a71ddfa59bdf12e5f603ad
oyintanda-zongwana <oyintandazongone@gmail.com>
2025-03-28 12:17:09 +0200

    refactor: reorder import statements in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import cors from 'cors';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import workflowRoutes from './routes/workflow.js';
 
